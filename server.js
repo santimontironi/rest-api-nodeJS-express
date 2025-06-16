dd48aa92fc73197b8ad574fe3cb115261adeafc8
santimontironi <santiimontironi@gmail.com>
2025-06-16 00:54:26 -0300

    metodos GET y POST funcionando

--- server.js
+++ server.js
@@ -3,16 +3,34 @@ const morgan = require('morgan')
 
 const app = express()
 
-const products = []
+const products = [
+    {
+        "id":1,
+        "name":"laptop",
+        "price":3000
+    }
+]
 
 app.use(morgan('dev'))
+app.use(express.json())
 
 app.get('/products',(req,res) => {
-    res.send("Obteniendo productos")
+    res.json(products)
+})
+
+app.get('/products/:id',(req,res) => {
+    const product = products.find(x => x.id === parseInt(req.params.id))
+    if(product){
+        res.json(product)
+    }else{
+        res.send('Product not found.')
+    }
 })
 
 app.post('/products',(req,res) => {
-    res.send("Creando productos")
+    const newProduct = {...req.body,id:products.length + 1}
+    products.push(newProduct)
+    res.send(newProduct)
 })
 
 app.put('/products',(req,res) => {
@@ -21,10 +39,6 @@ app.put('/products',(req,res) => {
 
 app.delete('/products',(req,res) => {
     res.send("Eliminando productos")
-})
-
-app.get('/products:id',(req,res) => {
-    res.send(`Obteniendo el producto${req.params.id}`)
 })
 
 app.listen(3000)
